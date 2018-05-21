2e246299ca648c28cecf9416cff523a395c12177
adityasrivastva <33947379+adityasrivastva@users.noreply.github.com>
2018-05-21 14:12:44 +0530

    Add files via upload
    Spring Rest CRUD

---
+++ RestCRUD/src/main/java/com/aditya/techunt/rest/model/User.java
@@ -0,0 +1,94 @@
+package com.aditya.techunt.rest.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+//@JsonIgnoreProperties({"age"})
+@JsonPropertyOrder({"age", "name", "id", "salary"})
+public class User {
+	
+	@JsonProperty("user_id")
+	private long id;
+	
+	private String name;
+	
+	private int age;
+	
+	private double salary;
+
+	public User(){
+		id=0;
+	}
+	
+	public User(long id, String name, int age, double salary){
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", age=" + age
+				+ ", salary=" + salary + "]";
+	}
+
+
+}
